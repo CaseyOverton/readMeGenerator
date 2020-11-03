d44cd090960161ab2bace6fdb7f44739838c0c68
Casey Overton <overtonc1@gmail.com>
2020-11-03 15:30:59 -0500

    made the readme file with correct syntax

--- README.md
+++ README.md
@@ -1,56 +1,34 @@
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
-        <title>Document</title>
-        <style>
-            body {
-                font-family: 'Open Sans Condensed', sans-serif;
-                background-color: grey;
-                color: black;
-            }
-            #tableOfContents {
-                text-align: center;
-            }
-            #description {
-                text-align: center;
-            }
-            h1 {
-                text-align: center;
-            }
-             #bodyInfo{
-                text-align: left;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>d</h1><br>
-        <div id='description'>
-        <h2>Project Description: </h2><h3>d</h3><br>
-        </div>  
-    
-        <div id=tableOfContents>
-        <h1>Table of Contents </h1>
-            <p>
-            <a id="installation">Installation</a><br>
-            <a id="license">License</a><br>
-            <a id="usage">Usage</a><br>
-            <a id="contribution">Contribution</a><br>
-            <a id="tests">Test Installation</a><br>
-            <a id="contact">Contact Me/Questions</a><br><br>
-        </p>
-    </div>
-    
-    <div id='bodyInfo'>
-        <h2 id="installation">Installation: undefined</h2><br>
-        <h2 id="license">License: MIT</h2><br>
-        <h2 id='usage'>Usage: d</h2><br>
-        <h2 id='contribution'>Contribution Guidelines: d</h2><br>
-        <h2 id='tests'>Test Instructions: d</h2><br>
-        <p id='contact'>Questions? Contact Me @ <a href="github.com/d">My Github Profile</a><br> Email: d</p>
-    </div>
-    </body>
-    </html>
+  # d
+   
+  # Table of Contents
+
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contribution](#Contribution)
+* [Testing](#Tests)
+* [License](#License)
+* [Questions](#Questions)
+
+## Description: 
+dd
+
+## Installation:
+ d
+
+## License:
+ None
+
+## Usage: 
+ dd
+
+## Contribution Guidelines:
+  d
+
+## Test Instructions: 
+d
+
+## Questions:
+ Contact Me @ github.com/d 
+ Email: d
--- index.js
+++ index.js
@@ -30,11 +30,11 @@ const writeFileAsync = util.promisify(fs.writeFile);
     type: "list",
     name: "license",
     message: "What license would you like?",
-    choices: ["MIT", "BSD 3", "Apache 2.0", "GNU"] 
+    choices: ["None", "MIT", "BSD 3", "Apache 2.0", "GNU"] 
     },
 {
     type: 'input', 
-    name: 'dependencies',
+    name: 'installation',
    message: "What command should be run to install dependencies? "
 },
 {
@@ -57,61 +57,40 @@ const writeFileAsync = util.promisify(fs.writeFile);
     // const html = generateHTML(answers);
     // console.log(generateHTML(answers))
     let html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
-        <title>Document</title>
-        <style>
-            body {
-                font-family: 'Open Sans Condensed', sans-serif;
-                background-color: grey;
-                color: black;
-            }
-            #tableOfContents {
-                text-align: center;
-            }
-            #description {
-                text-align: center;
-            }
-            h1 {
-                text-align: center;
-            }
-             #bodyInfo{
-                text-align: left;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>${answers.project}</h1><br>
-        <div id='description'>
-        <h2>Project Description: </h2><h3>${answers.description}</h3><br>
-        </div>  
-    
-        <div id=tableOfContents>
-        <h1>Table of Contents </h1>
-            <p>
-            <a id="installation">Installation</a><br>
-            <a id="license">License</a><br>
-            <a id="usage">Usage</a><br>
-            <a id="contribution">Contribution</a><br>
-            <a id="tests">Test Installation</a><br>
-            <a id="contact">Contact Me/Questions</a><br><br>
-        </p>
-    </div>
-    
-    <div id='bodyInfo'>
-        <h2 id="installation">Installation: ${answers.installation}</h2><br>
-        <h2 id="license">License: ${answers.license}</h2><br>
-        <h2 id='usage'>Usage: ${answers.toKnow}</h2><br>
-        <h2 id='contribution'>Contribution Guidelines: ${answers.contribution}</h2><br>
-        <h2 id='tests'>Test Instructions: ${answers.tests}</h2><br>
-        <p id='contact'>Questions? Contact Me @ <a href="github.com/${answers.github}">My Github Profile</a><br> Email: ${answers.email}</p>
-    </div>
-    </body>
-    </html>`
+  # ${answers.project}
+   
+  # Table of Contents
+
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contribution](#Contribution)
+* [Testing](#Tests)
+* [License](#License)
+* [Questions](#Questions)
+
+## Description: 
+${answers.description}
+
+## Installation:
+ ${answers.installation}
+
+## License:
+ ${answers.license}
+
+## Usage: 
+ ${answers.toKnow}
+
+## Contribution Guidelines:
+  ${answers.contribution}
+
+## Test Instructions: 
+${answers.tests}
+
+## Questions:
+ Contact Me @ github.com/${answers.github} 
+ Email: ${answers.email}
+`
     fs.writeFileSync('readme.md', html);
 })
  
